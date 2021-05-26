973653316047ddb57d0c55efe0ce805db38284e2
bao <nvbao1984@gmail.com>
2021-05-27 00:56:47 +0800

    Collection preview and collection item

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import HomePage from './pages/homepage.component';
+import { Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
+
 function App() {
   return (
     <div>
-      <HomePage/>
+    <Route exact path='/' component={HomePage}/>
+    <Route path='/shop' component={ShopPage}/>
     </div>
   );
 }
